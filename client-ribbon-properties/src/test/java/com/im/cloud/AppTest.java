11c60c3d659be32174a4a16fee78fd2c81ecb5dd
zlfkyo@sina.com <zlfkyo@sina.com>
2017-05-22 18:11:15 +0800

    调 hystrix

--- client-ribbon-properties/src/test/java/com/im/cloud/AppTest.java
+++ client-ribbon-properties/src/test/java/com/im/cloud/AppTest.java
@@ -1,38 +1,21 @@
 package com.im.cloud;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+public class AppTest{
+    public static void main(String[] args) {
+        Map<String,Integer> m = ImmutableMap.of("a",1,"b",2,"c",3);
+        m.entrySet().stream().reduce((k,v)->{
+            System.out.println(k);
+            System.out.println(v);
+            System.out.println();
+            return v;
+        });
     }
 }
