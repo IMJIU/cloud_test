b80b10fccd53ec38660f716eb96e89bca3f1e335
zlfkyo@sina.com <zlfkyo@sina.com>
2017-05-31 14:12:48 +0800

    config-client

---
+++ config-client2/src/main/java/com/im/cloud/ConfigClient2App.java
@@ -0,0 +1,15 @@
+package com.im.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ConfigClient2App {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient2App.class, args);
+    }
+}
---
+++ config-client2/src/main/java/com/im/cloud/controller/Controller.java
@@ -0,0 +1,24 @@
+package com.im.cloud.controller;/**
+ * Created by zhoulf on 2017/5/31.
+ */
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 这边的@RefreshScope注解不能少，否则即使调用/refresh，配置也不会刷新
+ * @author eacdy
+ */
+@RestController
+@RefreshScope
+public class Controller {
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return this.profile;
+    }
+}
